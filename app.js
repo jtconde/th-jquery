f452b7ab2f021eaf8f54e7b1b5794a7acb618193
jtconde <juliantconde@gmail.com>
2017-12-12 15:05:33 -0500

    improved application
    updated adopt cards animation, updated form

--- app.js
+++ app.js
@@ -10,3 +10,35 @@ $('.loc').hover(
         $(this).html("<strong>Location:</strong> Treehouse Adoption Center");
     }
 );
+
+// Add a pet to the page with user input
+$('#add-pet').on('click', function()
+{
+    var $name = $('#pet-name');
+    var $species = $('#pet-species');
+    var $notes = $('#pet-notes');
+
+    // Assemble HTML with new elements
+
+    var $newPet = $(
+        '<section class="six columns"><div class="card">' +
+        '<p><strong>Name:</strong> ' + $name.val() + '</p>' +
+        '<p><strong>Species:</strong> ' + $species.val() + '</p>' +
+        '<p><strong>Notes:</strong> ' + $notes.val() + '</p>' +
+        '<span class="close">&times;</span></div></section>'
+    );
+
+    // Attach new element to page
+
+    $('#posted-pets').append($newPet);
+
+});
+
+// Puppy images fade in
+$('img').css('display','none').fadeIn(1600);
+
+//
+
+$('.card').on('click', function() {
+    $(this).toggleClass('selected')
+})
